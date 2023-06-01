d1196763aad0e352456a9d797d9a8e82828d26c1
健一郎 木村 <k-kimura@alterbooth.com>
2023-06-01 15:15:15 +0900

    [add] add action

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "ID_OF_INPUT" ]
+then
+  echo "No id_of_input environment variable supplied"
+  exit 1
+fi
+if [ -z "ID_OF_INPUT2" ]
+then
+  echo "No id_of_input2 environment variable supplied"
+  exit 1
+fi
+
+echo "id_of_output=${ID_OF_INPUT}" >> "${GITHUB_OUTPUT}"
+echo "id_of_output2=${ID_OF_INPUT2}" >> "${GITHUB_OUTPUT}"
+cat "${GITHUB_OUTPUT}"
